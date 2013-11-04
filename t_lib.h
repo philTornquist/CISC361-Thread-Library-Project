9a6e048836d1d6b31897216b8d163d33684d46fd
Sean Moir <seanmoir@udel.edu>
2013-11-03 20:03:55 -0500

    revised t_lib.h

--- t_lib.h
+++ t_lib.h
@@ -1,7 +1,19 @@
 /*
  * types used by thread library
  */
+
+#ifndef T_LIB_H
+#define T_LIB_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ucontext.h>
 #include <sys/mman.h>
+
+int t_create(void (*fct)(void), int id, int pri);
+void t_init();
+void t_shutdown();
+int t_terminate();
+void t_yield();
+
+#endif /* T_LIB_H */
