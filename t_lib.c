e4cffa049e53cd42a5e7e92ba654e8049849e7a4
Sean Moir <seanmoir@udel.edu>
2013-11-03 19:49:12 -0500

    fixed more...

--- t_lib.c
+++ t_lib.c
@@ -20,7 +20,7 @@ void t_yield()
   running = ready;
   ready = tmp;
 
-  swapcontext(ready->thread_context, running->thread_context);
+  swapcontext(&ready->thread_context, &running->thread_context);
 }
 
 void t_init()
@@ -28,24 +28,24 @@ void t_init()
   tcb *tmp;
   tmp = (tcb *) malloc(sizeof(tcb));
 
-  getcontext(tmp->thread_context);    /* let tmp be the context of main() */
+  getcontext(&tmp->thread_context);    /* let tmp be the context of main() */
   running = tmp;
 }
 
-int t_create(void (*fct)(int), int id, int pri)
+int t_create(void (*fct)(void), int id, int pri)
 {
   size_t sz = 0x10000;
 
   tcb *uc;
   uc = (tcb *) malloc(sizeof(tcb));
 
-  getcontext(uc->thread_context);
+  getcontext(&uc->thread_context);
   uc->thread_context.uc_stack.ss_sp = mmap(0, sz,
        PROT_READ | PROT_WRITE | PROT_EXEC,
        MAP_PRIVATE | MAP_ANON, -1, 0);
   uc->thread_context.uc_stack.ss_size = sz;
   uc->thread_context.uc_stack.ss_flags = 0;
   uc->thread_context.uc_link = &running->thread_context; 
-  makecontext(uc->thread_context, fct, 1, id);
+  makecontext(&uc->thread_context, fct, 1, id);
   ready = uc;
 }
