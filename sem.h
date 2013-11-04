4473d9d7b241897d0e2e89361af50ad71e3f64e7
Philip Tornquist <philip@udel.edu>
2013-11-03 20:25:34 -0500

    Added semaphore support

---
+++ sem.h
@@ -0,0 +1,12 @@
+struct sem_t {
+	int sem_count;
+	tcb *block_queue;
+	tcb *end_queue;
+};
+
+typedef struct sem_t sem_t
+
+int sem_init(sem_t **sp, int sem_count);
+void sem_wait(sem_t *sp);
+void sem_signal(sem_t *sp);
+void sem_destroy(sem_t **sp);
