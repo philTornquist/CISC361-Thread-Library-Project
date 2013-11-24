1bd73f4bb19ec479f6cc8b2c33b346dfceca5c95
Sean Moir <seanmoir@udel.edu>
2013-11-24 18:55:03 -0500

    Implemented mbox_deposit

--- mbox.c
+++ mbox.c
@@ -6,6 +6,7 @@
 */
 
 #include "mbox.h"
+#include "t_lib.h"
 
 int mbox_create(mbox **mb)
 {
@@ -22,22 +23,46 @@ void mbox_destroy(mbox **mb)
 
 void mbox_deposit(mbox *mb, char *msg, int len)
 {
-	
+	mbox_tid_deposit(mb, 0, msg, len);
 }
 
-void mbox_block_deposit(mbox *mb, char *msg, int len)
+void mbox_tid_deposit(mbox *mb, int receiver, char *msg, int len)
 {
+	messageNode *tmp = malloc(sizeof(messageNode));
+	tmp->message = malloc(sizeof(char)*len+1);
+	memcpy(tmp->message, msg, len + 1);
+	tmp->len = len;
+	tmp->next = NULL;
+	tmp->blocked = NULL;
+	tmp->sender = running->thread_id;
+	tmp->receiver = receiver;
 
+	if (mb->msg == NULL) {
+		mb->msg = tmp;
+		return;
+	}
+
+	messageNode *end = mb->msg;
+	while (end->next != NULL)
+		end = end->next;
+	end->next = tmp;
+}
+
+void mbox_block_deposit(mbox *mb, int receiver, char *msg, int len)
+{
+	mbox_tid_deposit(mb, receiver, msg, len);
+	sem_init(&tmp->blocked, 0);
+	sem_wait(tmp->blocked);
 }
 
 void mbox_withdraw(mbox *mb, char *msg, int *len)
 {
-
+	mbox_tid_withdraw(mb, 0, msg, len);
 }
 
 void mbox_tid_withdraw(mbox *mb, int *tid, char *msg, int *len)
 {
-
+	
 }
 
 void mbox_block_withdraw(mbox *mb, int *tid, char *msg, int *len)
